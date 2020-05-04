4807e4637b72a453060ac2fd0d27583030029218
Madalin Antonoiu <antonoiumadalin@gmail.com>
2020-05-04 20:00:52 +0300

    Move Within Weapon Range! ( with no ability to stop doing it bug on)

--- Assets/Scripts/Movement/Mover.cs
+++ Assets/Scripts/Movement/Mover.cs
@@ -4,25 +4,35 @@ using UnityEngine.AI;
 namespace RPG.Movement {
 	public class Mover : MonoBehaviour {
    
-	[SerializeField] Transform target;
+		[SerializeField] Transform target;
 
-	// Update is called every frame
-	void Update(){    
-		UpdateAnimator();
+		NavMeshAgent navMeshAgent;
+
+		private void Start(){
+			navMeshAgent = GetComponent<NavMeshAgent>();
+		}
+
+    void Update(){    
+			UpdateAnimator();
+		}
+
+		public void MoveTo(Vector3 destination){
+			navMeshAgent.destination = destination;
+      navMeshAgent.isStopped = false;
+		}
+		
+
+		public void Stop(){
+			navMeshAgent.isStopped = true;
+		}
+
+		private void UpdateAnimator(){
+			Vector3 velocity = navMeshAgent.velocity;
+			Vector3 localVelocity = transform.InverseTransformDirection(velocity); // InverseTransformDirection - "No matter where you are in the world, lets convert that into animator for e.g you're moving forward at 3units"
+			float speed = localVelocity.z;
+			GetComponent<Animator>().SetFloat("forwardSpeed", speed); // Animator - Parameters - forwardSpeed exactly!
+		}
+
 	}
-
-	public void MoveTo(Vector3 destination){
-		GetComponent<NavMeshAgent>().destination = destination;
-	}
-  
-
-	private void UpdateAnimator(){
-		Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
-		Vector3 localVelocity = transform.InverseTransformDirection(velocity); // InverseTransformDirection - "No matter where you are in the world, lets convert that into animator for e.g you're moving forward at 3units"
-		float speed = localVelocity.z;
-		GetComponent<Animator>().SetFloat("forwardSpeed", speed); // Animator - Parameters - forwardSpeed exactly!
-	}
-
-}
 
 }
