3059edbbd28e581538939a339c9abe730188e324
Madalin Antonoiu <antonoiumadalin@gmail.com>
2020-05-04 09:22:59 +0300

    Auto Moving to target via AI

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour {
+   
+    [SerializeField] Transform target;
+    Ray lastRay;
+
+    void Update(){
+
+       
+       GetComponent<NavMeshAgent>().destination = target.position;
+    }
+}
