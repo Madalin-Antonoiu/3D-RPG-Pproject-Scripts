ad58fea3a40f07b7ee78e8384cec7f9d5fa271a9
Madalin Antonoiu <antonoiumadalin@gmail.com>
2020-05-05 12:13:17 +0300

    Waypoint Loops and Drawing lines

--- Assets/Scripts/Control/PatrolPath.cs
+++ Assets/Scripts/Control/PatrolPath.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace RPG.Control{
 
@@ -9,9 +7,23 @@ namespace RPG.Control{
     const float waypointGizmoRadius = 0.3f;
 
     private void OnDrawGizmos() {
-        for (int i = 0; i < transform.childCount; i++){
-            Gizmos.DrawSphere(transform.GetChild(i).position, waypointGizmoRadius); 
-        } 
+        for (int i = 0; i < transform.childCount; i++) {
+
+        int j = GetNextIndex(i);
+        Gizmos.DrawSphere(GetWaypoint(i), waypointGizmoRadius);
+		Gizmos.DrawLine(GetWaypoint(i), GetWaypoint(j));
+      }
+    }
+
+    private int GetNextIndex(int i) {
+		if(i +1 >= transform.childCount){
+			return 0;
+		}
+		return i + 1;
+    }
+
+    private Vector3 GetWaypoint(int i) {
+      return transform.GetChild(i).position;
     }
   }
 }
