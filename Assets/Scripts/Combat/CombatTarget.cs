bc06533029e903bb90350dcbef5612845a2f605c
Madalin Antonoiu <antonoiumadalin@gmail.com>
2020-05-05 08:25:23 +0300

    Tidy Up Attacks on Enemies

--- Assets/Scripts/Combat/CombatTarget.cs
+++ Assets/Scripts/Combat/CombatTarget.cs
@@ -2,6 +2,10 @@ using UnityEngine;
 
 namespace RPG.Combat{
 
+  // Whenever we place CombatTarget script, it is going to automatically place a Health component too
+  // "You can't have a CombatTarget without Health!"
+  [RequireComponent(typeof(Health))]
+
   public class CombatTarget : MonoBehaviour {
     
   }
--- Assets/Scripts/Combat/Health.cs
+++ Assets/Scripts/Combat/Health.cs
@@ -2,11 +2,29 @@ using UnityEngine;
 
 namespace RPG.Combat {
   public class Health : MonoBehaviour {
-    [SerializeField] float health = 100f;
+
+    [SerializeField] float healthPoints = 100f;
+
+    bool isDead = false;
+
+    //Public getter method, returns true or false
+    public bool IsDead(){
+      return isDead;
+    }
 
     public void TakeDamage(float damage){
-      health = Mathf.Max(health-damage, 0);// if it goes < 0, 0 will be higher and will take the vaue of 0;
-      print(health); 
+      healthPoints = Mathf.Max(healthPoints - damage, 0);// if it goes < 0, 0 will be higher and will take the vaue of 0;
+      if(healthPoints == 0) {
+        Die();
+      }
+      print(healthPoints); 
+    }
+
+    private void Die() {
+      if(isDead) return;
+
+      isDead = true;
+      GetComponent<Animator>().SetTrigger("die");
     }
   }
 }
